6648059dee8e22fa493a448eb6551fa249727beb
Baklogr <polpoliver518359@gmail.com>
2026-09-01 17:17:35 +0300

    Update main.py

--- Payload_Type/my_agent/main.py
+++ Payload_Type/my_agent/main.py
@@ -1,8 +1,4 @@
 import mythic_container
-from Payload_Type.my_agent.agent_functions.builder import MyAgent
+from agent_functions.builder import MyAgent
 
-
-# This service is loaded by the Mythic container runtime.
-# The `start_and_run_forever` call keeps the service alive and
-# handles registration with Mythic.
 mythic_container.mythic_service.start_and_run_forever()
